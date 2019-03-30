e839fb8eb22b8847ffb8a694c329808b344ac1e1
MisterSquishy <pdavids@comcast.net>
2019-03-30 12:06:33 -0400

    authenticate correctly

--- webapp/settings.py
+++ webapp/settings.py
@@ -127,7 +127,7 @@ django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
 }
